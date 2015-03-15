cef6194d897aec6e033f8664cb77620bb5dcd887
Kadi Kraman <hellokadi@gmail.com>
2015-03-15 14:52:19 +0000

    Added check for number of cats returned for random selection

--- server.py
+++ server.py
@@ -3,20 +3,18 @@ import requests
 from random import randint
 
 
+GIPHY_CAT_URL = "http://api.giphy.com/v1/gifs/search?q=cute+cat&api_key=dc6zaTOxFJmzC&limit=100"
+
+
 app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 
 @app.route("/cats/next")
 def next_cat():
-    giphy_cat_url = "http://api.giphy.com/v1/gifs/search?q=cute+cat&api_key=dc6zaTOxFJmzC&limit=100"
-    response = requests.get(giphy_cat_url)
+    response = requests.get(GIPHY_CAT_URL)
     json = response.json()
-    rand = randint(0, 99)
+    num_responses = len(json['data'])  # get number of cats returned (for the off-chance it's less than the 100 cap)
+    rand = randint(0, num_responses - 1)
     image_url = json['data'][rand]['images']['original']['mp4']
     return image_url
 
